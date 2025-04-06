983cdef54fedfbbe97cf6155b232101dad50448e
AlvaroDey <71075540@continental.edu.pe>
2025-04-06 08:31:18 -0500

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function hamburg(){
+function hamburg() {
     const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(0px)"
+    navbar.style.transform = "translateY(0px)"
 }
-function cancel(){
+
+function cancel() {
     const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(-500px)"
+    navbar.style.transform = "translateY(-500px)"
 }
 
 // for Typewriter effect
@@ -21,26 +22,32 @@ let textIndex = 0;
 let charcterIndex = 0;
 
 function typeWriter() {
-    if(charcterIndex < texts[textIndex].length){
+    if (charcterIndex < texts[textIndex].length) {
         textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
         charcterIndex++;
-        setTimeout(typeWriter, speed); 
-    }
-    else{
+        setTimeout(typeWriter, speed);
+    } else {
         setTimeout(eraseText, 1000)
     }
 }
 
 function eraseText() {
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1)
-        setTimeout(eraseText, 50)
-    }
-    else{
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
+        // Verifica que solo se repita el texto una vez
         textIndex = (textIndex + 1) % texts.length;
         charcterIndex = 0;
-        setTimeout(typeWriter,500)
+        setTimeout(typeWriter, 500);
     }
 }
 
-window.onload = typeWriter;
+// Evita que el efecto se ejecute múltiples veces
+window.onload = function() {
+    // Solo ejecuta la función typeWriter una vez al cargar la página
+    if (textElements.innerHTML === "") {
+        typeWriter();
+    }
+}
+
